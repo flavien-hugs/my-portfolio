cbdfbd0cf38b6912b3f94602f973ec0d53a3f647
flavien-hugs <geekactux@gmail.com>
2022-08-14 17:41:59 +0000

    create and edit contact application

---
+++ core/contact/forms.py
@@ -0,0 +1,40 @@
+""" Contact forms"""
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField, SelectField
+from wtforms.validators import InputRequired, DataRequired, Length, Email
+
+from .models import Contact
+
+SUBJECT_CHOICE = [
+    (1, "Je souhaite prendre un rendez-vous avec toi"),
+    (2, "J'aimerais plus d'informations sur tes prestations"),
+    (3, "Pourais-je avoir des d'informations sur tes séances de formations"),
+    (4, "J'aimerais t'engager sur un projet"),
+]
+
+
+class ContactForm(FlaskForm):
+    fullname = StringField(
+        "Nom & prénoms", validators=[Length(min=4, max=80), InputRequired(), DataRequired()]
+    )
+    email = StringField(
+        'Adresse Email',
+        validators=[
+            Length(min=4, max=80), DataRequired(), InputRequired(),
+            Email(message='Entrer une adresse email valide.')
+        ]
+    )
+    phone = StringField(
+        "Téléphone",
+        validators=[
+            Length(min=4, max=15), InputRequired(), DataRequired()
+        ]
+    )
+    subject = SelectField(
+        "Sujet",
+        choices=SUBJECT_CHOICE, coerce=int,
+        validators=[InputRequired(), DataRequired()]
+    )
+    message = TextAreaField("Votre message", validators=[InputRequired(), DataRequired()])
+    submit = SubmitField("Envoyer le message")
---
+++ core/contact/models.py
@@ -0,0 +1,23 @@
+""" Contact app models """
+
+from datetime import datetime
+
+from .. import db
+from ..utils import Updateable
+
+
+class Contact(Updateable, db.Model):
+    """Contact model"""
+
+    __tablename__ = 'contact'
+
+    id = db.Column(db.Integer, primary_key=True)
+    message = db.Column(db.Text, nullable=False)
+    phone = db.Column(db.Integer, nullable=False)
+    email = db.Column(db.String(80), nullable=False)
+    subject = db.Column(db.String(80), nullable=False)
+    fullname = db.Column(db.String(80), nullable=False)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow())
+
+    def __repr__(self):
+        return f"Contact(id={self.id!r}, email={self.email!r})"
