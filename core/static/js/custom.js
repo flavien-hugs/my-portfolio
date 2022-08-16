100f60cd33fec3c80422063d4b378b47fb068f55
flavien-hugs <geekactux@gmail.com>
2022-08-16 20:01:39 +0000

    improve static file

--- core/static/js/custom.js
+++ core/static/js/custom.js
@@ -1,25 +1,8 @@
-/*
-
-=========================================================
-* Leo Agency Bootstrap Theme
-=========================================================
-
-* Product Page: https://themesberg.com/product/bootstrap/leo-agency-bootstrap-4-template
-* Copyright 2020 Themesberg (https://www.themesberg.com)
-
-* Coded by https://themesberg.com
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
-
 "use strict";
 
 $(document).ready(function () {
 
-    
+
     // preloader
     var $preloader = $('.preloader');
     if($preloader.length) {
@@ -76,18 +59,6 @@ $(document).ready(function () {
         $(this).parent('.dropdown-submenu').toggleClass('show');
     });
 
-    // Headroom - show/hide navbar on scroll
-    if ($('.headroom')[0]) {
-        var headroom = new Headroom(document.querySelector("#navbar-main"), {
-            offset: 0,
-            tolerance: {
-                up: 1,
-                down: 0
-            },
-        });
-        headroom.init();
-    }
-
     // Background images for sections
     $('[data-background]').each(function () {
         $(this).css('background-image', 'url(' + $(this).attr('data-background') + ')');
@@ -95,18 +66,6 @@ $(document).ready(function () {
 
     $('[data-background-color]').each(function () {
         $(this).css('background-color', $(this).attr('data-background-color'));
-    });
-
-    $('[data-color]').each(function () {
-        $(this).css('color', $(this).attr('data-color'));
-    });
-
-    // Datepicker
-    $('.datepicker')[0] && $('.datepicker').each(function () {
-        $('.datepicker').datepicker({
-            disableTouchKeyboard: true,
-            autoclose: false
-        });
     });
 
     // Tooltip
@@ -148,67 +107,13 @@ $(document).ready(function () {
         }(e)
     })
 
-
-    // NoUI Slider
-    if ($(".input-slider-container")[0]) {
-        $('.input-slider-container').each(function () {
-
-            var slider = $(this).find('.input-slider');
-            var sliderId = slider.attr('id');
-            var minValue = slider.data('range-value-min');
-            var maxValue = slider.data('range-value-max');
-
-            var sliderValue = $(this).find('.range-slider-value');
-            var sliderValueId = sliderValue.attr('id');
-            var startValue = sliderValue.data('range-value-low');
-
-            var c = document.getElementById(sliderId),
-                d = document.getElementById(sliderValueId);
-
-            noUiSlider.create(c, {
-                start: [parseInt(startValue)],
-                connect: [true, false],
-                //step: 1000,
-                range: {
-                    'min': [parseInt(minValue)],
-                    'max': [parseInt(maxValue)]
-                }
-            });
-
-            c.noUiSlider.on('update', function (a, b) {
-                d.textContent = a[b];
-            });
-        })
-    }
-
-    $(".progress-bar").each(function () {
-        $(this).waypoint(function () {
-            var progressBar = $(".progress-bar");
-            progressBar.each(function (indx) {
-                $(this).css("width", $(this).attr("aria-valuenow") + "%");
-            });
-            $('.progress-bar').css({
-                animation: "animate-positive 3s",
-                opacity: "1"
-            });
-        }, {
-                triggerOnce: true,
-                offset: '60%'
-            });
-    });
-
-    // lightbox init
-    lightbox.option({
-        'resizeDuration': 200,
-        'wrapAround': true
-    });
-
     //Owl Carousel
     $('.testimonial-carousel').owlCarousel({
         loop: true,
         margin: 8,
         nav: false,
         dots: true,
+        autoplay: true,
         responsive: {
             0: {
                 items: 1
@@ -261,114 +166,4 @@ $(document).ready(function () {
             $rotating_card_container.addClass('hover');
         }
     });
-
-    //Countdown
-    $('#clock').countdown('2020/10/10').on('update.countdown', function (event) {
-        var $this = $(this).html(event.strftime(''
-            + '<span>%-w</span> week%!w '
-            + '<span>%-d</span> day%!d '
-            + '<span>%H</span> hr '
-            + '<span>%M</span> min '
-            + '<span>%S</span> sec'));
-    });
-
-    //Parallax
-    $('.jarallax').jarallax({
-        speed: 0.2
-    });
-
-    //Smooth scroll
-    var scroll = new SmoothScroll('a[href*="#"]', {
-        speed: 500,
-        speedAsDuration: true
-    });
-
-    // Equalize height to the max of the elements
-    if ($(document).width() >= breakpoints.lg) {
-
-        // object to keep track of id's and jQuery elements
-        var equalize = {
-            uniqueIds: [],
-            elements: []
-        };
-
-        // identify all unique id's
-        $('[data-equalize-height]').each(function () {
-            var id = $(this).attr('data-equalize-height');
-            if (!equalize.uniqueIds.includes(id)) {
-                equalize.uniqueIds.push(id)
-                equalize.elements.push({ id: id, elements: [] });
-            }
-        });
-
-        // add elements in order
-        $('[data-equalize-height]').each(function () {
-            var $el = $(this);
-            var id = $el.attr('data-equalize-height');
-            equalize.elements.map(function (elements) {
-                if (elements.id === id) {
-                    elements.elements.push($el);
-                }
-            });
-        });
-
-        // equalize
-        equalize.elements.map(function (elements) {
-            var elements = elements.elements;
-            if (elements.length) {
-                var maxHeight = 0;
-
-                // determine the larget height
-                elements.map(function ($element) {
-                    maxHeight = maxHeight < $element.outerHeight() ? $element.outerHeight() : maxHeight;
-                });
-
-                // make all elements with the same [data-equalize-height] value
-                // equal the larget height
-                elements.map(function ($element) {
-                    $element.height(maxHeight);
-                })
-            }
-        });
-    }
-
-    // update target element content to match number of characters
-    $('[data-bind-characters-target]').each(function () {
-        var $text = $($(this).attr('data-bind-characters-target'));
-        var maxCharacters = parseInt($(this).attr('maxlength'));
-        $text.text(maxCharacters);
-
-        $(this).on('keyup change', function (e) {
-            var string = $(this).val();
-            var characters = string.length;
-            var charactersRemaining = maxCharacters - characters;
-            $text.text(charactersRemaining);
-        })
-    });
-
-    // copy docs
-    $('.copy-docs').on('click', function () {
-        var $copy = $(this);
-        var htmlEntities = $copy.parents('.nav-wrapper').siblings('.card').find('.tab-pane:last-of-type').html();
-        var htmlDecoded = $('<div/>').html(htmlEntities).text().trim();
-
-        var $temp = $('<textarea>');
-        $('body').append($temp);
-        console.log(htmlDecoded);
-        $temp.val(htmlDecoded).select();
-        document.execCommand('copy');
-        $temp.remove();
-
-        $copy.text('Copied!');
-        $copy.addClass('copied');
-
-        setTimeout(function () {
-            $copy.text('Copy');
-            $copy.removeClass('copied');
-        }, 1000);
-    });
-
-    $('.current-year').text(new Date().getFullYear());
-
 });
-
